21971f959f8386aa3878cf795348d676478dc80a
Eduard Balakh <eduardbalakh@gmail.com>
2020-08-18 20:43:43 +0500

    test

--- Program.cs
+++ Program.cs
@@ -80,29 +80,5 @@ namespace ApplicationMonitor
 
             Console.ReadKey(true);
         }
-        //public static string GetActiveTabUrl()
-        //{
-        //    Process[] procsChrome = Process.GetProcessesByName("chrome");
-        //    foreach (Process chrome in procsChrome)
-        //    {
-        //        if (chrome.MainWindowHandle == IntPtr.Zero)
-        //            continue;
-
-        //        AutomationElement element = AutomationElement.FromHandle(chrome.MainWindowHandle);
-        //        if (element == null)
-        //            return null;
-        //        Condition conditions = new AndCondition(
-        //            new PropertyCondition(AutomationElement.ProcessIdProperty, chrome.Id),
-        //            new PropertyCondition(AutomationElement.IsControlElementProperty, true),
-        //            new PropertyCondition(AutomationElement.IsContentElementProperty, true),
-        //            new PropertyCondition(AutomationElement.ControlTypeProperty, ControlType.Edit));
-
-        //        AutomationElement elementx = element.FindFirst(System.Windows.Automation.TreeScope.Descendants, conditions);
-        //        return ((ValuePattern)elementx.GetCurrentPattern(ValuePattern.Pattern)).Current.Value as string;
-        //    }
-
-        //    return "end";
-        //}
-
     }
 }
